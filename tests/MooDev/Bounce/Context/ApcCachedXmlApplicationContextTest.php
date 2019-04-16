56fac9d76162a3d4e7844ef2f819592b68616135
Rob Aiken <rob.aiken@moo.com>
2019-04-16 11:45:37 +0100

    deprecated APC

--- tests/MooDev/Bounce/Context/ApcCachedXmlApplicationContextTest.php
+++ tests/MooDev/Bounce/Context/ApcCachedXmlApplicationContextTest.php
@@ -20,6 +20,7 @@ class ApcCachedXmlApplicationContextTest extends TestCase
      */
     public function testTestConfig()
     {
+        $this->markTestSkipped();
         $this->assertTrue(function_exists("apc_fetch"), "APC module is not loaded.");
         $this->assertTrue(ini_get("apc.enable_cli") == true, "apc.enable_cli must be enabled in your php.ini");
     }
